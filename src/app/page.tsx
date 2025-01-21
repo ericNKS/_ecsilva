b2683f3936a9f670bbeb6669f5b26af8845173eb
ericNKS <ek.silva.santos@gmail.com>
2025-01-21 17:08:36 -0300

    cleanup: removing wrong import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Contact from "./components/contact/contact";
-import Footer from "./components/footer/footer";
 import Page from "./components/home/home";
 import Navbar from "./components/navigation/navbar";
 import Project from "./components/Project/project";
